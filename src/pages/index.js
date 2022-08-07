e80a93e9a2ff99c5072463a24ca6bbebb7be7d27
Razze <razzeee@gmail.com>
2022-08-07 16:20:51 +0200

    Cleanup imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout title={`Welcome`} description="Flatpak documentation">
       <main
