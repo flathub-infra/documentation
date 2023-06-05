134b18652756cf87af3847334b309d4b439a3d4e
Kolja Lampe <razzeee@gmail.com>
2023-06-05 16:48:39 +0200

    Update repo link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,15 +36,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/flathub/flathub-docs-docusaurus/tree/main/",
+          editUrl: "https://github.com/flathub/documentation/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/flathub/flathub-docs-docusaurus/tree/main/",
+          editUrl: "https://github.com/flathub/documentation/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
