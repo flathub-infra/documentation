d30ec609d72a38a18315ac8d9b38c74f3a490658
Kolja Lampe <razzeee@gmail.com>
2023-06-05 13:37:38 +0200

    Change baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://docs.flathub.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/documentation/",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
