6066ed28f3c4495da287e62aecafe2eb00a209b8
Kolja Lampe <razzeee@gmail.com>
2023-06-05 19:53:06 +0200

    Respect color theme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,6 +71,9 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
       metadata: [
         {
           name: "keywords",
