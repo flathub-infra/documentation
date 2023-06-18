342d3616170fc17727fcf49f6c00990f5f81a0a3
Razze <razzeee@gmail.com>
2023-06-18 20:52:46 +0200

    Use logo for social cards

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,6 +71,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: "img/logo.svg",
       colorMode: {
         respectPrefersColorScheme: true,
       },
