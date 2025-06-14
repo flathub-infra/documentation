0b33514681992de2dad61f7a93c8fe423967951e
Razze <razzeee@gmail.com>
2025-06-14 15:29:40 +0200

    Update to tailwind 4

--- plugins/tailwind-plugin.cjs
+++ plugins/tailwind-plugin.cjs
@@ -2,11 +2,7 @@ function tailwindPlugin(context, options) {
   return {
     name: "tailwind-plugin",
     configurePostCss(postcssOptions) {
-      postcssOptions.plugins = [
-        require("postcss-import"),
-        require("tailwindcss"),
-        require("autoprefixer"),
-      ];
+      postcssOptions.plugins = [require("@tailwindcss/postcss")];
       return postcssOptions;
     },
   };
