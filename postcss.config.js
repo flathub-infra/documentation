62267fe11a19856676489ebaa71aca9c08ca04bf
Kolja Lampe <razzeee@gmail.com>
2025-12-18 19:43:17 +0100

    build: update swiper imports and adjust logo size in HeroBanner component

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true,
+      },
+    },
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
